1dc0325ccdca8eaefce45b37e30d48fc402fa715
Jamil Ur Rehman Ahmadzai <jamilurrehman722@gmail.com>
2021-02-26 04:45:13 +0100

    implemented add user to database

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,7 +1,32 @@
 const Userdb = require("../model/model");
 
 //create new user
-exports.create = (req, res) => {};
+exports.create = (req, res) => {
+  //validate request
+  if (!req.body) {
+    return res.status(404).send({ message: "Content cannot be empty" });
+  }
+
+  //new user
+  const user = new Userdb({
+    name: req.body.name,
+    email: req.body.email,
+    gender: req.body.gender,
+    status: req.body.status,
+  });
+
+  //save to mongodb
+  user
+    .save(user)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Error occured in create operation",
+      });
+    });
+};
 
 //get all users
 exports.find = (req, res) => {};
