c266fe278dfc398f43470eff299bfbd585123193
Jamil Ur Rehman Ahmadzai <jamilurrehman722@gmail.com>
2021-02-28 19:59:50 +0100

    get the data from database and display on website

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const axios = require("axios");
 
 const route = express.Router();
 const controller = require("../controller/controller");
 
 route.get("/", (req, res) => {
-  res.render("index");
+  axios
+    .get("http://localhost:3000/api/users")
+    .then((response) => {
+      return res.render("index", { users: response.data, id: 0 });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 });
 
 route.get("/add-user", (req, res) => {
