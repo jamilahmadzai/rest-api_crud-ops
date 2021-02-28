eedf57d907e58fc1359304aa348a652074b3cb26
Jamil Ur Rehman Ahmadzai <jamilurrehman722@gmail.com>
2021-02-28 22:29:07 +0100

    delete a user from website

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,3 +20,22 @@ $("#update-user").submit((e) => {
     alert("Data Updated successfully");
   });
 });
+
+if (window.location.pathname == "/") {
+  $ondelete = $(".table tbody td a.delete");
+  $ondelete.click(function () {
+    var id = $(this).attr("data-id");
+
+    var request = {
+      url: `http://localhost:3000/api/users/${id}`,
+      method: "DELETE",
+    };
+
+    if (confirm("Do you really want to delete this record?")) {
+      $.ajax(request).done((response) => {
+        alert("Data Deleted Successfully!");
+        location.reload();
+      });
+    }
+  });
+}
